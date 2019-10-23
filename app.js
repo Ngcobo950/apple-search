62998422491640faff425d333df6bbc14f7cd62d
Musa <ngcobomusa950@gmail.com>
2019-10-23 09:37:47 +0200

    fix app.js

--- app.js
+++ app.js
@@ -25,11 +25,12 @@ app.use(function(req, res, next) {
 app.use(cookieParser());
 
 
-if(process.env.NODE_ENV === 'poduction'){
-   app.use(express.static('client/build'));
-   app.get('/', function(req, res){
-       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-   });
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('/', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+    });
 }
 
 app.use('/', indexRouter);
